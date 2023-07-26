3f65e8e6688f89f36f4874304880f8b5d46aefc6
rvkathar <97075829+rvkathar@users.noreply.github.com>
2023-07-26 14:20:38 +0530

    Assignment 2

---
+++ assignment 2/app.js
@@ -0,0 +1,17 @@
+console.log("hello")
+let person = {
+
+}
+person.name = "john";
+person.age = 30;
+person.city = "New York";
+
+console.log(person);
+delete person.age;
+console.log(person);
+
+person.job = "Engineer";
+console.log(person);
+
+person.city = "San Francisco";
+console.log(person);
